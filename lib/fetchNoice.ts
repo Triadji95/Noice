6aeda76f51f491c126447c0ed6579d247d65c858
Triadji95 <triadji95@gmail.com>
2025-05-23 02:47:12 +0700

    Add files via upload

--- lib/fetchNoice.ts
+++ lib/fetchNoice.ts
@@ -1,1 +1,8 @@
-
+export async function fetchNoiceData() {
+  // Dummy data for now
+  return [
+    { date: '2025-05-20', count: 12 },
+    { date: '2025-05-21', count: 18 },
+    { date: '2025-05-22', count: 9 },
+  ];
+}
